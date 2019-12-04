768ff278f83f8f24f79e55ccbeb506600155b6ae
dczerwin <58464555+dczerwin@users.noreply.github.com>
2019-12-04 12:48:10 +0100

    Fraszka małego ptaszka

---
+++ app/src/main/java/com/example/carsharing/Login.java
@@ -0,0 +1,46 @@
+package com.example.carsharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
+
+public class Login extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView Login = (TextView) FindViewById(R.id.Logintxt);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
