a30fd4989c9b7b49ef029714213becc628cc7643
dczerwin <daniel.czerwinski@sng.com.pl>
2019-12-17 15:12:38 +0100

    sasas

--- app/src/main/java/com/example/carsharing/FirebaseMessagingService.java
+++ app/src/main/java/com/example/carsharing/FirebaseMessagingService.java
@@ -22,6 +22,10 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Log.d("Firebase","****************");
+        Log.d("Firebase","Pacz dziala");
+        Log.d("Firebase","****************");
         if (remoteMessage.getData().size() > 0) {
             try {
                 JSONObject data = new JSONObject(remoteMessage.getData());
